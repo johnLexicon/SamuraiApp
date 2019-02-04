44a6bd1a3568e05934bbb0c9c85be5ace5e67aad
John Lundgren <john.paul.lundgren@gmail.com>
2019-02-04 16:42:59 +0100

    First commit.

---
+++ SamuraiApp.Data/Migrations/20190204152333_SwordLength.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class SwordLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SwordLength",
+                table: "Samurais",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SwordLength",
+                table: "Samurais");
+        }
+    }
+}
---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        //Solution using ASP.NET DI.
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(sb => new { sb.BattleId, sb.SamuraiId });
+            //modelBuilder.Entity<Samurai>().Property(s => s.SecretIdentity).IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //Solution without DI
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SamuraiDbContext;Trusted_Connection=True;MultipleActiveResultSets=true");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
